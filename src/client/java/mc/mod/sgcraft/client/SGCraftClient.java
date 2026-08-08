1dfd7848a5d8f914354dd047b0fcef75b1f845e3
kanes48 <kanes23589@gmail.com>
2026-08-08 21:53:27 +0200

    Initial commit with template

---
+++ src/client/java/mc/mod/sgcraft/client/SGCraftClient.java
@@ -0,0 +1,10 @@
+package mc.mod.sgcraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SGCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
